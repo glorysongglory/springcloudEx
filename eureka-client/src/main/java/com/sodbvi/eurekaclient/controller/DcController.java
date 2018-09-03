da9870834f2956bd01b324acf54940d0c34f7e62
sodbvi <317878410@qq.com>
2018-09-03 16:04:43 +0800

    init

---
+++ eureka-client/src/main/java/com/sodbvi/eurekaclient/controller/DcController.java
@@ -0,0 +1,27 @@
+package com.sodbvi.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sodbvi
+ * Date: 2018-08-27 -16:34
+ */
+
+@RestController
+public class DcController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        String services = "Services: " + discoveryClient.getServices();
+        System.out.println(services);
+        return services;
+    }
+
+}
